e2c65bde951e8ebefd11c28d195f38c5a3195631
DegtyarevMikhail <logindegtyarev@mail.ru>
2021-12-15 00:23:06 +0300

    EndOfDay

--- app/src/main/java/com/example/samsungtest/MainActivity.java
+++ app/src/main/java/com/example/samsungtest/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.samsungtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn0 = (Button) findViewById(R.id.Button11);
+        Button btn1 = (Button) findViewById(R.id.Button01);
+        Button btn2 = (Button) findViewById(R.id.Button02);
+        Button btn3 = (Button) findViewById(R.id.Button03);
+        Button btn4 = (Button) findViewById(R.id.Button04);
+        Button btn5 = (Button) findViewById(R.id.Button05);
+        Button btn6 = (Button) findViewById(R.id.Button06);
+        Button btn7 = (Button) findViewById(R.id.Button07);
+        Button btn8 = (Button) findViewById(R.id.Button08);
+        Button btn9 = (Button) findViewById(R.id.Button09);
+
+        Button btnPlus = (Button) findViewById(R.id.ButtonPlus);
+        Button btnMinus = (Button) findViewById(R.id.ButtonMinus);
+        Button btnDivision = (Button) findViewById(R.id.ButtonDivide);
+        Button btnMultiply = (Button) findViewById(R.id.ButtonMul);
+        Button btnEqual = (Button) findViewById(R.id.Button12);
+
+
     }
 }
